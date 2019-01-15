b01ffaf6b58ba8175e9426936a81bfc16365be4f
zelboraf <uminski.krzysztof@gmail.com>
2019-01-15 15:04:48 +0100

    PlanDao

---
+++ src/main/java/pl/coderslab/model/Plan.java
@@ -0,0 +1,69 @@
+package pl.coderslab.model;
+
+import java.sql.Date;
+
+public class Plan {
+
+	private int id;
+	private String name;
+	private String description;
+	private Date created;
+	private int admin_id;
+
+	@Override
+	public String toString() {
+		return "Plan [id=" + id + ", name=" + name + ", description=" + description + ", created=" + created +
+				", admin_id=" + admin_id + "]";
+	}
+
+	public Plan() {}
+
+	public Plan(int id, String name, String description, Date created, int admin_id) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.created = created;
+		this.admin_id = admin_id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public int getAdmin_id() {
+		return admin_id;
+	}
+
+	public void setAdmin_id(int admin_id) {
+		this.admin_id = admin_id;
+	}
+
+}
