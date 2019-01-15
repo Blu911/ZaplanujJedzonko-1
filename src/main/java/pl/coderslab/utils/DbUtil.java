2d8ceb78668ca3a376c17d11f98a0f932378b1e7
Cutter72 <drelich_pawel@o2.pl>
2019-01-15 12:35:41 +0100

    Tymczasowa zmiana kodu DbUtil

--- src/main/java/pl/coderslab/utils/DbUtil.java
+++ src/main/java/pl/coderslab/utils/DbUtil.java
@@ -5,24 +5,41 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//public class DbUtil {
+//    private static DataSource dataSource;
+//
+//    public static Connection getConnection() throws SQLException {
+//        return getInstance().getConnection();
+//    }
+//
+//    private static DataSource getInstance() {
+//        if (dataSource == null) {
+//            try {
+//                Context context = new InitialContext();
+//                dataSource = (DataSource) context.lookup("java:comp/env/jdbc/scrumlab");
+//            } catch (NamingException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return dataSource;
+//    }
+//}
+
+//Działąjący kod:
 public class DbUtil {
-    private static DataSource dataSource;
+    private static String DB_URL = "jdbc:mysql://localhost:3306/scrumlab?useSSL=false&useUnicode=yes&characterEncoding=UTF-8";
+    private static String DB_USER = "root";
+    private static String DB_PASS = "coderslab";
 
     public static Connection getConnection() throws SQLException {
-        return getInstance().getConnection();
-    }
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception e) {
 
-    private static DataSource getInstance() {
-        if (dataSource == null) {
-            try {
-                Context context = new InitialContext();
-                dataSource = (DataSource) context.lookup("java:comp/env/jdbc/scrumlab");
-            } catch (NamingException e) {
-                e.printStackTrace();
-            }
         }
-        return dataSource;
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
     }
 }
