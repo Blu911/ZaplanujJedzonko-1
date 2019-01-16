dfc47d2e0ceebc1af5364ba0953c1d99b6994fb6
Krystian Woźniak <krys.blu@gmail.com>
2019-01-16 11:56:49 +0100

    Handling the login form

--- src/main/java/pl/coderslab/web/LoginServlet.java
+++ src/main/java/pl/coderslab/web/LoginServlet.java
@@ -1,12 +1,19 @@
 package pl.coderslab.web;
 
 
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -15,9 +22,17 @@ public class LoginServlet extends HttpServlet {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-/**To Test - unhide **/
-//        response.getWriter().append("Witaj " + email +" " + password);
-
+/** Checking if inserted email and password is in database
+ *  If yes opening session and redirecting to HomePage
+ *  If not showing LoginPage**/
+        List<Admin> adminList = AdminDao.findAll();
+        for (Admin admin : adminList) {
+            if (Objects.equals(admin.getEmail(), email) && BCrypt.checkpw(password, admin.getPassword())) {
+                response.sendRedirect(request.getContextPath() + "/index.jsp");
+                return;
+            }
+        }
+        doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
