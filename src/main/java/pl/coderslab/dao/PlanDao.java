27bd91abda3754987257d02eb5a345deeb369004
Cutter72 <drelich_pawel@o2.pl>
2019-01-17 16:24:46 +0100

    Removed SQL query 'scrumlab' prefix

--- src/main/java/pl/coderslab/dao/PlanDao.java
+++ src/main/java/pl/coderslab/dao/PlanDao.java
@@ -19,7 +19,7 @@ public class PlanDao {
     private static final String FIND_ALL_PLANS_QUERY = "SELECT * FROM plan";
     private static final String READ_PLAN_QUERY = "SELECT * from plan where id = ?";
     private static final String UPDATE_PLAN_QUERY = "UPDATE	plan SET name = ? , description = ?, created = ?, admin_id = ? WHERE id = ?";
-    private static final String COUNT_USER_PLANS = "SELECT COUNT(*) FROM scrumlab.plan WHERE scrumlab.plan.admin_id = ?";
+    private static final String COUNT_USER_PLANS = "SELECT COUNT(*) FROM plan WHERE plan.admin_id = ?";
 
     /**
      * Get plan by id
