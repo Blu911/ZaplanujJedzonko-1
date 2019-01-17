fab4e4ba4bad1d1da8f1e26f66fe8179bb7c851a
zelboraf <uminski.krzysztof@gmail.com>
2019-01-17 11:51:27 +0100

    adden bitten machen

--- src/main/java/pl/coderslab/web/AddScheduleServlet.java
+++ src/main/java/pl/coderslab/web/AddScheduleServlet.java
@@ -1,21 +1,38 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.PlanDao;
+import pl.coderslab.dao.RecipeDao;
+import pl.coderslab.model.Plan;
+import pl.coderslab.model.Recipe;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.Timestamp;
 
 @WebServlet("/app/plan/add")
 public class AddScheduleServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		HttpSession session = request.getSession();
+
+		String name = request.getParameter("planName");
+		String description = request.getParameter("planDescription");
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		int adminId = (int) session.getAttribute("user_id");
+
+		PlanDao.create(new Plan(name, description, timestamp, adminId));
+		response.sendRedirect(request.getContextPath() + "/app/plan/list");
+
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		getServletContext().getRequestDispatcher("/add-schedule.jsp").forward(request, response);
+		getServletContext().getRequestDispatcher("/appPlanAdd.jsp").forward(request, response);
 
 	}
 }
